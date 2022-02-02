1512782ac559f7698ab14d8178f79476e6d4e914
Kilder Ferreira Colvalan <85182805+Colvalan@users.noreply.github.com>
2022-02-02 00:28:32 -0300

    Add files via upload

---
+++ modulo_intermediario/aulas/vendas/calc_preco.py
@@ -0,0 +1,17 @@
+from modulo_intermediario.aulas.vendas.formata import preco
+
+
+def aumento(valor, porcentagem, formata=False):
+    r = valor + (valor * (porcentagem / 100))
+    if formata:
+        return preco.real(r)
+    else:
+        return r
+
+
+def reducao(valor, porcentagem, formata=False):
+    r = valor - (valor * (porcentagem / 100))
+    if formata:
+        return preco.real(r)
+    else:
+        return r
